28fc468de3df6dad69a38c765f0a11fe5a4ee0a8
AndersonFLB <andersonlosada.11@gmail.com>
2025-03-30 23:55:49 -0500

    add ice cream purchase alerts based on user input in code.js

--- Ex1/code.js
+++ Ex1/code.js
@@ -1,3 +1,19 @@
 dineroCofla = prompt("¿Cuánto dinero tienes Cofla?");
 dineroRoberto = prompt("¿Cuánto dinero tienes Roberto?");
 dineroPedro = prompt("¿Cuánto dinero tienes Pedro?");
+
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+  alert("Cómprate el helado de agua.");
+} else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+  alert("Cómprate el helado de crema.");
+} else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+  alert("Cómprate el helado de heladix.");
+} else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+  alert("Cómprate el helado de heladovich.");
+} else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+  alert("Cómprate el helado de helardo.");
+} else if (dineroCofla >= 2.9) {
+  alert("Helado con confites o el pote de 1/4kg.");
+} else {
+  alert("No tienes dinero para comprar helado.");
+}
