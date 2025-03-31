89b1c482dfffd8e8438ed594987e5e451d5d54ee
AndersonFLB <andersonlosada.11@gmail.com>
2025-03-30 23:59:37 -0500

    add ice cream purchase alerts for Pedro in code.js

--- Ex1/code.js
+++ Ex1/code.js
@@ -33,6 +33,7 @@ if (dineroCofla >= 0.6 && dineroCofla < 1) {
 } else {
   alert("No tienes dinero para comprar helado.");
 }
+
 if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
   alert("Cómprate el helado de agua.");
 } else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
@@ -44,6 +45,22 @@ if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
 } else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
   alert("Cómprate el helado de helardo.");
 } else if (dineroRoberto >= 2.9) {
+  alert("Helado con confites o el pote de 1/4kg.");
+} else {
+  alert("No tienes dinero para comprar helado.");
+}
+
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+  alert("Cómprate el helado de agua.");
+} else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+  alert("Cómprate el helado de crema.");
+} else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+  alert("Cómprate el helado de heladix.");
+} else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+  alert("Cómprate el helado de heladovich.");
+} else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+  alert("Cómprate el helado de helardo.");
+} else if (dineroPedro >= 2.9) {
   alert("Helado con confites o el pote de 1/4kg.");
 } else {
   alert("No tienes dinero para comprar helado.");
