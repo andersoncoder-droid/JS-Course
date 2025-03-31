92bbb09dfdcc795be388cdf62fce219747ac5a52
AndersonFLB <andersonlosada.11@gmail.com>
2025-03-31 17:30:54 -0500

    add remaining balance alerts for ice cream selection in code.js

--- Ex1/code.js
+++ Ex1/code.js
@@ -4,16 +4,22 @@ dineroPedro = prompt("¿Cuánto dinero tienes Pedro?");
 
 if (dineroCofla >= 0.6 && dineroCofla < 1) {
   alert("Cofla, cómprate el helado de agua.");
+  alert("y te sobra " + dineroCofla - 0.6);
 } else if (dineroCofla >= 1 && dineroCofla < 1.6) {
   alert("Cofla, cómprate el helado de crema.");
+  alert("y te sobra " + dineroCofla - 1);
 } else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
   alert("Cofla, cómprate el helado de heladix.");
+  alert("y te sobra " + dineroCofla - 1.6);
 } else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
   alert("Cofla, cómprate el helado de heladovich.");
+  alert("y te sobra " + dineroCofla - 1.7);
 } else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
   alert("Cofla, cómprate el helado de helardo.");
+  alert("y te sobra " + dineroCofla - 1.8);
 } else if (dineroCofla >= 2.9) {
   alert("Cofla, cómprate un helado con confites o el pote de 1/4kg.");
+  alert("y te sobra " + dineroCofla - 2.9);
 } else {
   alert("Cofla ,no tienes dinero para comprar helado.");
 }
