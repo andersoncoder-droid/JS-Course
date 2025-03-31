7c9d37341872bf4c70b752f31ecc6bf435da81b8
AndersonFLB <andersonlosada.11@gmail.com>
2025-03-31 00:01:39 -0500

    remove redundant ice cream purchase alerts for Cofla in code.js

--- Ex1/code.js
+++ Ex1/code.js
@@ -18,22 +18,6 @@ if (dineroCofla >= 0.6 && dineroCofla < 1) {
   alert("Cofla ,no tienes dinero para comprar helado.");
 }
 
-if (dineroCofla >= 0.6 && dineroCofla < 1) {
-  alert("Cómprate el helado de agua.");
-} else if (dineroCofla >= 1 && dineroCofla < 1.6) {
-  alert("Cómprate el helado de crema.");
-} else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
-  alert("Cómprate el helado de heladix.");
-} else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
-  alert("Cómprate el helado de heladovich.");
-} else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
-  alert("Cómprate el helado de helardo.");
-} else if (dineroCofla >= 2.9) {
-  alert("Helado con confites o el pote de 1/4kg.");
-} else {
-  alert("No tienes dinero para comprar helado.");
-}
-
 if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
   alert("Cómprate el helado de agua.");
 } else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
